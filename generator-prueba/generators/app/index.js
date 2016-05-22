cdeab5a728531c3af92993d8555aa919245a92c1
Adrian Eguez <eadepto@hotmail.com>
2016-05-21 19:42:05 -0500

    Aumentar funcionalidad a crear pagina y completar el Readme

--- generator-prueba/generators/app/index.js
+++ generator-prueba/generators/app/index.js
@@ -1,28 +1,30 @@
 var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
-    //Corren en orden los metodos si tienen callback
-    primerMetodo: function () {
-        console.log('Desplegar Informacion del Generador');
-    },
-    traerMail: function () {
-        var done = this.async();
+  //Corren en orden los metodos si tienen callback
+  primerMetodo: function () {
+    console.log('Desplegar Informacion del Generador');
+  },
+  traerMail: function () {
+    var done = this.async();
 
-        function traerMailusuario(id) {
-            console.log('Se devuelve el correo del Usuario: ' + id);
-            setTimeout(function () {
-                var correo = {correo:'correo@mail.com'};
-                console.log('Se demoro 3 segundos en terminar');
-                console.log(correo);
-                done();
-            }, 3000)
+    function traerMailusuario(id) {
+      console.log('Se devuelve el correo del Usuario: ' + id);
+      setTimeout(function () {
+        var correo = {
+          correo: 'correo@mail.com'
+        };
+        console.log('Se demoro 3 segundos en terminar');
+        console.log(correo);
+        done();
+      }, 3000);
 
-        }
-
-        traerMailusuario(1);
-    },
-    decirChao: function () {
-        console.log('Gracias por usar el generador');
     }
+
+    traerMailusuario(1);
+  },
+  decirChao: function () {
+    console.log('Gracias por usar el generador');
+  }
 
 });
